55f4e7682b6e308db0f5d5c7cd5fd1df2c7bc765
Michael Ingold <mike.ingold@gmail.com>
2025-11-11 15:53:50 -0500

    Split map JSON into separate js file, minor tweaks

--- image_explorer/app_vB.js
+++ image_explorer/app_vB.js
@@ -14,37 +14,6 @@ function textbox_value_or_placeholder(el) {
 // DATA INITIALIZATION
 // ========================================
 
-const MAP_POLYGONS = [
-    {
-        "id": "1000",
-        "side": "upper",
-        "name": "Power Connector",
-        "description": "Connects to power cable.",
-        "coordinates": [[84, 23], [85, 256], [132, 256], [130, 238], [220, 237], [221, 24]]
-    },
-    {
-        "id": "1001",
-        "side": "upper",
-        "name": "USB Connector",
-        "description": "Connects to USB cable.",
-        "coordinates": [[538, 54], [533, 162], [693, 161], [690, 52]]
-    },
-    {
-        "id": "1002",
-        "side": "upper",
-        "name": "Reset Button",
-        "description": "Button to reset device.",
-        "coordinates": [[704, 229], [838, 231], [840, 105], [701, 112]]
-    },
-    {
-        "id": "1003",
-        "side": "upper",
-        "name": "CPU",
-        "description": "Main processor.",
-        "coordinates": [[354, 886], [461, 997], [566, 879], [455, 780]]
-    }
-];
-
 class MapState {
     constructor(image_url, polygons = []) {
         this.image_url = image_url;
---
+++ image_explorer/map.js
@@ -0,0 +1,44 @@
+const MAP_POLYGONS = [
+    {
+        "id": "1000",
+        "side": "upper",
+        "name": "Power Connector",
+        "description": "Connects to power cable.",
+        "coordinates": [[84, 23], [85, 256], [132, 256], [130, 238], [220, 237], [221, 24]]
+    },
+    {
+        "id": "1001",
+        "side": "upper",
+        "name": "USB Connector",
+        "description": "Connects to USB cable.",
+        "coordinates": [[538, 54], [533, 162], [693, 161], [690, 52]]
+    },
+    {
+        "id": "1002",
+        "side": "upper",
+        "name": "Reset Button",
+        "description": "Button to reset device.",
+        "coordinates": [[704, 229], [838, 231], [840, 105], [701, 112]]
+    },
+    {
+        "id": "1003",
+        "side": "upper",
+        "name": "CPU",
+        "description": "Main processor.",
+        "coordinates": [[354, 886], [461, 997], [566, 879], [455, 780]]
+    },
+    {
+        "id": "2001",
+        "side": "lower",
+        "name": "CPU",
+        "description": "Model ATMEGA328P-AU",
+        "coordinates": [[435, 820], [625, 820], [623, 424], [435, 424]]
+    },
+    {
+        "id": "2002",
+        "side": "lower",
+        "name": "Reset Switch",
+        "description": "Button for resetting system.",
+        "coordinates": [[259, 307], [412, 307], [417, 184], [261, 183]]
+    }
+];
